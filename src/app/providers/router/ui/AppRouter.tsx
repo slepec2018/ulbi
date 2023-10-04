d7ce817f473a1ad19791372564a02b8eda11b94f
slepec <slepec2018@gmail.com>
2023-10-04 12:24:22 +0100

    add_404_loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 
 const AppRouter = () => {
@@ -10,7 +11,7 @@ const AppRouter = () => {
         <Route
           key={path}
           element={(
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<PageLoader />}>
               <div className='page-wrapper'>
                 {element}
               </div>
